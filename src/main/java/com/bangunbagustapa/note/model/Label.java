0318331a1c20683c73ec851e87597ea1c95f2d99
Bangun Bagustapa <bangunbagustapa@gmail.com>
2024-09-29 20:50:28 +0700

    add label entity and migration

---
+++ src/main/java/com/bangunbagustapa/note/model/Label.java
@@ -0,0 +1,39 @@
+package com.bangunbagustapa.note.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "label")
+public class Label {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false)
+    private Long label_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String name;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updated_at;
+}
