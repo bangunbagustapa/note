b3f7c6bba81a81afda5bb43efd4efa4e59ec3343
Bangun Bagustapa <bangunbagustapa@gmail.com>
2024-09-29 19:58:07 +0700

    add user entity and migration

---
+++ src/main/java/com/bangunbagustapa/note/model/User.java
@@ -0,0 +1,39 @@
+package com.bangunbagustapa.note.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(updatable = false, nullable = false)
+    private UUID user_id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updated_at;
+}
